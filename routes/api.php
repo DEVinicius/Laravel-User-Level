6d22cedde768712737da9135d9b42c33ea43ba99
Vinicius Pereira <agavi2014@hotmail.com>
2020-11-30 22:56:08 -0300

    [NEW] JWT Finalizado

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/store', [UserController::class, 'store']);
+
+Route::group(['middleware' => "apiJwt"], function(){
 });
